db461229dc7b058923d966d0cfc89051b1e68328
Muhammed Shaker <muhammedshaker.me@gmail.com>
2024-11-28 21:18:40 +0100

    add change range feature

--- app.js
+++ app.js
@@ -19,18 +19,18 @@ function isPrime(num) {
 
 const  Header = createElement("header", {className: "header"},
   createElement("h1", null, "Numbers Facts"),
-  createElement("p", null, "1th fact: Numbers are everywhere..")
+  createElement("p", null, `1th fact: "Numbers are everywhere"`)
 );
 
 let previousValue = null;
 
 const App = () =>{
 
+  const [range, setRange] = useState(0) // 0 : 0 - 100 , 1: 100 - 200...
   const [fact, setFact] = useState("Tap a number, discover a fact!");
 
   async function  handleClick(event){
     try{
-
       const value = event.target.dataset.value;
       const response = await fetch(`http://numbersapi.com/${value}/math`);
       const factText = await response.text();
@@ -49,20 +49,35 @@ const App = () =>{
   }
 
   const numbers = [...Array(101).keys()].map(number =>{
+    const numberValue = number >= 0 ? number + (range + 1) * 100 : number - range * 100; 
+    
     return createElement(
     "div",
     {
-      className: `number ${number === 0 ? "zero" : getNatureOfnumber(number)}`,
+      className: `number ${numberValue === 0 ? "zero" : getNatureOfnumber(numberValue)}`,
       onClick: handleClick,
-      'data-value': number,
+      'data-value': numberValue,
     },
-    number);
+    numberValue);
   });
+
+  function handleBrowseClick(change){
+    setRange(prev => prev + change);
+    if(previousValue){
+      document.querySelector(".number.active").classList.remove("active");
+      previousValue = null; // when range changes the previous selected value is not an element of the current range
+                           // reset to null  fix returing null in querySelector [data-value="{previousValue}"]
+    }
+  }
 
   return createElement("div", {className: "app"},
     Header,
     createElement("p", {className: "fact"}, fact),
-    createElement("div", {className: "numbers-container"}, ...numbers)
+    createElement("div", {className: "numbers-container"}, ...numbers),
+    createElement("div", {className: "browse-ranges"}, 
+      createElement("button", {className: "browse-btn", onClick:() => handleBrowseClick(-1)}, `${(range - 1) * 100} to ${range * 100}`),
+      createElement("button", {className: "browse-btn", onClick:() => handleBrowseClick(1)}, `${range  * 100} to ${( range + 1) * 100}`)
+    )
   );
 }
 
